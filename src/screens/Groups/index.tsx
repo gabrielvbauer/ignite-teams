3d891213a27fe7b49e24037803dddc89c4aebc33
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-08-27 23:04:16 -0300

    button compoennt

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,6 +7,7 @@ import { Header } from '@components/Header'
 import { GroupCard } from '@components/GroupCard'
 import { FlatList } from 'react-native'
 import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -29,6 +30,8 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+
+      <Button title='Cadastrar nova turma' />
     </Container>
   )
 }
