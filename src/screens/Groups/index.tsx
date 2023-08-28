9f86592645f536f0a19f3d25e77ce86ae0488792
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-08-27 22:16:30 -0300

    feat: creating GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,12 +2,15 @@ import { Highlight } from "@components/Highlight"
 import { Container } from "./styles"
 
 import { Header } from "@components/Header"
+import { GroupCard } from "@components/GroupCard"
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do ignite" />
     </Container>
   )
 }
