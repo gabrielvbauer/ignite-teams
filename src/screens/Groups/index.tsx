f605f5f6e6a81d5ee645465dbee8bcccc33f0f8d
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-08-27 22:42:09 -0300

    feat: ListEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,16 +6,10 @@ import { Container } from './styles'
 import { Header } from '@components/Header'
 import { GroupCard } from '@components/GroupCard'
 import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
 
 export function Groups() {
-  const [groups, setGroups] = useState([
-    {
-      title: 'CC e Eng'
-    },
-    {
-      title: '3tentos'
-    }
-  ])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -24,11 +18,15 @@ export function Groups() {
 
       <FlatList
         data={groups}
-        keyExtractor={item => item.title}
+        keyExtractor={item => item}
         renderItem={({item}) => (
           <GroupCard
-            title={item.title}
+            title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
     </Container>
