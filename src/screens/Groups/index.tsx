33781a794dbecd28ba9ff13ec4543795a0b7c551
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-08-27 22:32:38 -0300

    feat: listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,36 @@
-import { Highlight } from "@components/Highlight"
-import { Container } from "./styles"
+import { useState } from 'react'
 
-import { Header } from "@components/Header"
-import { GroupCard } from "@components/GroupCard"
+import { Highlight } from '@components/Highlight'
+import { Container } from './styles'
+
+import { Header } from '@components/Header'
+import { GroupCard } from '@components/GroupCard'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState([
+    {
+      title: 'CC e Eng'
+    },
+    {
+      title: '3tentos'
+    }
+  ])
+
   return (
     <Container>
       <Header />
-      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
 
-      <GroupCard title="Galera do ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item.title}
+        renderItem={({item}) => (
+          <GroupCard
+            title={item.title}
+          />
+        )}
+      />
     </Container>
   )
 }
