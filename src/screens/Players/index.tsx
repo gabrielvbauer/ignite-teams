02b84fd35313733ea37cc1327d9f527388f82c72
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-08-28 22:27:40 -0300

    feat: Filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 
 export function Players() {
   return (
@@ -23,6 +24,9 @@ export function Players() {
           icon="add"
         />
       </Form>
+
+      <Filter title="Time A" isActive />
+      <Filter title="Time B" />
     </Container>
   )
 }
