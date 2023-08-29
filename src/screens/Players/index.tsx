6d964990ea6906a2790e265b4eebea8608ccba57
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-08-28 22:49:52 -0300

    feat: finished Players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,10 +7,12 @@ import { Filter } from "@components/Filter";
 import { FlatList } from "react-native";
 import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState(['Gabriel', 'João'])
+  const [players, setPlayers] = useState(['Gabriel', 'João', 'Martina', 'Laís', 'Andressa', 'André', 'Victor'])
 
   return (
     <Container>
@@ -58,6 +60,21 @@ export function Players() {
             onRemove={() => {}}
           />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Não há pessoas nesse time"
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
       />
     </Container>
   )
